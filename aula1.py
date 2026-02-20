5ad00a01fa0bc6885f5ed218eb3fab8dfc2a7da0
niczgalvao <nicolas.galvao.oliveira@escola.pr.gov.br>
2026-02-20 08:06:30 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,10 @@
+print( "Olá Mundo!")
+nome = "masculo foda"
+idade = 22
+#Usar o print com variaveis
+print("Olá, meu nome é" , nome, ", minha idade é," idade )
+
+#F-string
+print(f'olá sou o {nome} minha idade é {idade}')
+
+#Para executar escreva Python3 aula1.py
