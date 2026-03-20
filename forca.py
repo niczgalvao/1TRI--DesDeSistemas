5f9dee1f9275380e49eb9cf00aaba0e7f59a6b21
niczgalvao <nicolas.galvao.oliveira@escola.pr.gov.br>
2026-03-20 08:12:37 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,40 @@
+import random
+palavras = []
+arquivo = open("palavras.txt", "r")
+for linha in arquivo:
+    palavras.append(linha.strip())
+
+palavra = random.choice(palavras)
+
+letras_acertadas = []
+for letra in palavra:
+    letras_acertadas.append("_")
+    
+
+acertou = False
+enforcou = False
+tentativa = 1
+limite_tentativas = len(palavra) + 6
+def mostrarPalavra():
+    for letra in letras_acertadas:
+        print(letra, end=" ")
+    print("")
+while(not acertou and not enforcou):
+    mostrarPalavra()
+    chute = input("Digite uma letra:")
+    indice = 0
+    for letra in palavra:
+        if (chute.upper() == letra):
+            letras_acertadas[indice] = letra
+        indice = indice + 1
+
+    if (tentativa == limite_tentativas):
+        print("Você perdeu :(\nA palavra era:", palavra)
+        enforcou = True
+
+    if (letras_acertadas.count("_") == 0):
+        print("Parabéns! Você descobriu a palavra!")
+        mostrarPalavra()
+        acertou = True
+
+    tentativa = tentativa + 1
